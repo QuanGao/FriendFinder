6da665560840c468e015c2ed61d8fae583911096
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-03-02 23:51:05 -0600

    need readme and deploy

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@ let friendsArr = [
     {
         name: "Sailor moon",
         photos: "https://dw9to29mmj727.cloudfront.net/properties/2016/432-SeriesThumbnails_SM__400x320.jpg",
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: ["1","1","1","1","1","1","1","1","1","1"]
     },
     {
         name: "Sherlock Holmes",
         photos: "http://images4.fanpop.com/image/photos/15500000/Cute-Sherlock-and-Watson-sherlock-15536981-385-239.jpg",
-        scores: [2,2,2,2,2,2,2,2,2,2]
+        scores: ["2","2","2","2","2","2","2","2","2","2"]
     },
     {
-        name: "",
-        photos: "",
-        scores: [3,3,3,3,3,3,3,3,3,3]
+        name: "Saitama",
+        photos: "http://www.konbini.com/wp-content/blogs.dir/11/files/2017/04/onepunchman.jpg",
+        scores: ["3","3","3","3","3","3","3","3","3","3"]
     },
     {
-        name: "",
-        photos: "",
-        scores: [4,4,4,4,4,4,4,4,4,4]
+        name: "Pusheen",
+        photos: "https://cdn170.picsart.com/upscale-241091004033212.png?r1024x1024",
+        scores: ["4","4","4","4","4","4","4","4","4","4"]
     },
     {
         name: "Naruto",
         photos: "https://orig00.deviantart.net/3a9c/f/2012/191/c/d/naruto_cute_by_tania15-d56p6av.jpg",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        scores: ["5","5","5","5","5","5","5","5","5","5"]
     }
 ]
 
