c896c89175607f22d5524bb3305ad7abd2ffa9b1
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-03-03 18:18:54 -0600

    slim down logic.js

--- app/public/assets/javascript/logic.js
+++ app/public/assets/javascript/logic.js
@@ -1,62 +1,47 @@
- 
-    $(document).ready(function () {
+ $(document).ready(function () {
 
-        $("#submitSurvy").on("click", function (event) {
+     $("#submitSurvy").on("click", function (event) {
+         event.preventDefault();
 
-            event.preventDefault();
+         function sumAbs(arr1, arr2) {
+             let sum = 0;
+             for (let i = 0; i < arr1.length; i++) {
+                 sum += Math.abs(+arr1[i] - +arr2[i]);
+             };
+             return sum;
+         }
 
-            let isComplete = true;
+         let inputData = $("form").serializeArray().filter(a => {
+             return a.value.trim() !== ""
+         })
 
-            function sumAbs(arr1, arr2) {
-                let sum = 0;
-                for (let i = 0; i < arr1.length; i++) {
-                    sum += Math.abs(+arr1[i] - +arr2[i]);
-                };
-                return sum;
-            }
+         if (inputData.length === 12) {
 
-            $("input").each(function () {
-                if (!$(this).val()) {
-                    isComplete = false;
-                } 
-            });
+             let user = {
+                 uniqid: Date.now(),
+                 name: inputData[0].value,
+                 photos: inputData[1].value,
+                 scores: inputData.slice(2).map(a => {
+                     return a.value
+                 })
+             };
 
-            let user = {
-                name: $("#inputName").val().trim(),
-                photos: $("#inputPhoto").val().trim(),
-                uniqid: Date.now()
-            };
+             $.get("/api/friends", function (data) {
+                 let bff = data.reduce((a, b) => {
+                     return sumAbs(a.scores, user.scores) < sumAbs(b.scores, user.scores) ? a : b;
+                 })
+                 $(".bestMatch-name").text(bff.name);
+                 $(".bestMatch-img").html(`<img src=${bff.photos}></img>`)
+                 $.post("/api/friends", user, function (data) {
+                     if (data) {
+                         $("#result").modal("show");
+                     }
+                 });
+             })
+         } else {
+             alert("Please fill out all questions!");
+         }
 
-            user.scores = [];
-            $("select").each(function () {
-                let score = $(this).val();
-                if (!score) {
-                    isComplete = false;
-                } else {
-                    user.scores.push(score)
-                }
-            })
+     })
 
-            if (!isComplete) {
-                alert("Please fill out all questions!");
-            } else {
-                $.get("/api/friends", function (data) {
-                    let bff = data.reduce((a, b) => {
-                        return sumAbs(a.scores, user.scores) < sumAbs(b.scores, user.scores) ? a : b;                          
-                    })
-
-                    $(".bestMatch-name").text(bff.name);
-                    $(".bestMatch-img").html(`<img src=${bff.photos}></img>`)
-
-                    $.post("/api/friends", user, function (data) {
-                        if (data) {
-                            $("#result").modal("show");
-                        } 
-                    });
-
-                })
-            }
-
-        })
-
-    })
+ })
